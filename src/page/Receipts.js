96f68a0e2f609eb33e6070c9aab97894ceaea709
adhanmiah <120992876+adhanmiah@users.noreply.github.com>
2024-04-03 17:56:49 +0100

    Update Receipts.js

--- src/page/Receipts.js
+++ src/page/Receipts.js
@@ -4,6 +4,10 @@ import { GlobalContext } from '../context/GlobalState';
 const Receipts = () => {
   const { receipts } = useContext(GlobalContext);
 
+  const getFileNameWithoutExtension = (filename) => {
+    return filename.split('.').slice(0, -1).join('.');
+  };
+
   const openReceiptInNewTab = (receipt) => {
     const newWindow = window.open('', '_blank');
     if (newWindow) {
@@ -28,7 +32,7 @@ const Receipts = () => {
       <ul>
         {receipts.map((receipt, index) => (
           <li key={index}>
-            <p>{receipt.name}</p>
+            <p>Filename: {getFileNameWithoutExtension(receipt.name)}</p>
             <button onClick={() => openReceiptInNewTab(receipt)}>View Receipt</button>
           </li>
         ))}
