4b8aa4c9d81fa2e0ed73f4af00834c9ea5447a87
emircodes <151061600+emircodes@users.noreply.github.com>
2024-04-07 16:30:26 +0100

    Main Widgets Dashboard.js

--- src/page/Dashboard.js
+++ src/page/Dashboard.js
@@ -3,6 +3,10 @@ import { GlobalContext } from '../context/GlobalState';
 import "../page/Dashboard.css";
 import { Pie } from 'react-chartjs-2';
 
+
+import {Chart, ArcElement} from 'chart.js'
+Chart.register(ArcElement);
+
 const Dashboard = () => {
   
   const { transactions } = useContext(GlobalContext);
@@ -57,104 +61,151 @@ const Dashboard = () => {
 
   /* pie chart - Start */
 
-  const expensesByCurrency = transactions.reduce((acc, transaction) => {
+  
+  const expensesByCategory = transactions.reduce((acc, transaction) => {
     const { expenseType, value } = transaction;
     acc[expenseType] = (acc[expenseType] || 0) + Number(value);
     return acc;
   }, {});
+
   
+  const categoryColors = ['#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0'];
+
   
+  const totalExpenses = Object.values(expensesByCategory).reduce((acc, value) => acc + value, 0);
+
   const pieChartData = {
-    labels: Object.keys(expensesByCurrency),
+    labels: Object.keys(expensesByCategory),
     datasets: [{
-      data: Object.values(expensesByCurrency),
-      backgroundColor: [
-        '#FF6384', '#36A2EB', '#FFCE56', 
-        // ...
-      ],
-      hoverBackgroundColor: [
-        '#FF6384', '#36A2EB', '#FFCE56', 
-        // ...
-      ]
+      data: Object.values(expensesByCategory).map(value => ((value / totalExpenses) * 100).toFixed(2)),
+      backgroundColor: categoryColors,
+      hoverOffset: 4
     }]
   };
+
+  const pieChartOptions = {
+    maintainAspectRatio: false,
+    legend: {
+      display: false, 
+    },
+    
+  };
+  
+
+  
 
   /* pie chart - End */
   
 
   return (
-    <div className="grid-two-item grid-common Expenses-Dashboard">
-        <div className="grid-c-title">
-            <h3 className="grid-c-title-text">Expenses</h3>
-            <button className="grid-c-title-icon">
-                {/* Placeholder for button icon */}
-            </button>
-        </div>
-        <div className="grid-c-top text-silver-v1">
-            <h2 className="lg-value">Total </h2>
-            <span className="lg-value">£ {total.toFixed(2)}</span> 
-        </div>
-        <div className="grid-c4-content bg-jet">
-            <div className="grid-items">
-                {
-                    
-                    transactions.map((transaction) => (
-                        <div className="grid-item" key={transaction.id}>
-                            <div className="grid-item-l">
-                                <div className="icon">
-                                    {/* Placeholder for item icon */}
-                                </div>
-                                <p className="text text-silver-v1">{transaction.expenseName} <span>{transaction.expenseType}</span></p>
-                            </div>
-                            <div className="grid-item-r">
-                                <span className="text-silver-v1">£ {Number(transaction.value).toFixed(2)}</span>
-                            </div>
-                        </div>
-                    ))
-                }
+
+    <div className="dashboard-container" style={{ display: 'flex', justifyContent: 'space-between', padding: '20px' }}>
+      <div className="grid-two-item grid-common Expenses-Dashboard" style={{ marginRight: '10px' }}>
+          <div className="grid-c-title">
+              <h3 className="grid-c-title-text">Expenses</h3>
+              <button className="grid-c-title-icon">
+                  {/* Place for button icon */}
+              </button>
+          </div>
+          <div className="grid-c-top text-silver-v1">
+              <h2 className="lg-value">Total </h2>
+              <span className="lg-value">£ {total.toFixed(2)}</span> 
+          </div>
+          <div className="grid-c4-content bg-jet">
+              <div className="grid-items">
+                  {
+                      
+                      transactions.map((transaction) => (
+                          <div className="grid-item" key={transaction.id}>
+                              <div className="grid-item-l">
+                                  <div className="icon">
+                                      {/* Place for item icon */}
+                                  </div>
+                                  <p className="text text-silver-v1">{transaction.expenseName} <span>{transaction.expenseType}</span></p>
+                              </div>
+                              <div className="grid-item-r">
+                                  <span className="text-silver-v1">£ {Number(transaction.value).toFixed(2)}</span>
+                              </div>
+                          </div>
+                      ))
+                  }
+              </div>
+          </div>
+
+          <div className="Currency-Conversion-Section grid-common">
+          <h1>Exchange Rates</h1>
+          <div className="grid-c-title">
+            <select
+              value={targetCurrency}
+              onChange={handleTargetCurrencyChange}
+              className="currency-dropdown text-silver-v1"
+            >
+              <option value="">Select a currency</option>
+              {currencies.map((currency) => (
+                <option key={currency} value={currency}>{currency}</option>
+              ))}
+            </select>
+          </div>
+          {loading ? (
+            <p className="text-silver-v1">Loading...</p>
+          ) : error ? (
+            <p className="text-scarlet">{error}</p>
+          ) : exchangeRates && (
+            <div className="grid-c4-content">
+              <h2>Base: {baseCurrency}, Amount: {total.toFixed(2)}, Target: {targetCurrency}</h2>
+              <ul>
+                {exchangeRates && targetCurrency in exchangeRates.rates ? (
+                  <li className="text-green">
+                    {targetCurrency}: {(exchangeRates.rates[targetCurrency] * total).toFixed(2)}
+                  </li>
+                ) : (
+                  <p className="text-scarlet">Target currency not available in rates.</p>
+                )}
+              </ul>
             </div>
+          )}
         </div>
 
-        <div className="Currency-Conversion-Section grid-common">
-        <h1>Exchange Rates</h1>
-        <div className="grid-c-title">
-          <select
-            value={targetCurrency}
-            onChange={handleTargetCurrencyChange}
-            className="currency-dropdown text-silver-v1"
-          >
-            <option value="">Select a currency</option>
-            {currencies.map((currency) => (
-              <option key={currency} value={currency}>{currency}</option>
-            ))}
-          </select>
-        </div>
-        {loading ? (
-          <p className="text-silver-v1">Loading...</p>
-        ) : error ? (
-          <p className="text-scarlet">{error}</p>
-        ) : exchangeRates && (
-          <div className="grid-c4-content">
-            <h2>Base: {baseCurrency}, Amount: {total.toFixed(2)}, Target: {targetCurrency}</h2>
-            <ul>
-              {exchangeRates && targetCurrency in exchangeRates.rates ? (
-                <li className="text-green">
-                  {targetCurrency}: {(exchangeRates.rates[targetCurrency] * total).toFixed(2)}
-                </li>
-              ) : (
-                <p className="text-scarlet">Target currency not available in rates.</p>
-              )}
-            </ul>
-          </div>
-        )}
+      
+
+        {/*<Loans/>*/}
+
+        {/*
+        <div className="pie-chart-widget" style={{ backgroundColor: '#333', color: '#fff', marginLeft: '20px' }}>
+          <h3>Expenses by Currency Type</h3>
+          <Pie data={pieChartData} options={{ maintainAspectRatio: false }} />
+        </div>  
+                */}
+
       </div>
 
-      {/*
-      <div className="pie-chart-widget" style={{ backgroundColor: '#333', color: '#fff', marginLeft: '20px' }}>
-        <h3>Expenses by Currency Type</h3>
-        <Pie data={pieChartData} options={{ maintainAspectRatio: false }} />
-      </div>  
-              */}
+      {/* Pie Chart widget */}
+    <div className="grid-common pie-chart-widget">
+      <h3>Expenses by Category</h3>
+      <div style={{ height: '300px' }}>
+        <Pie data={pieChartData} options={pieChartOptions} />
+      </div>
+      {/* Legend Part */}
+      <div className="chart-legend">
+        {pieChartData.labels.map((label, index) => {
+          const value = pieChartData.datasets[0].data[index];
+          return (
+            <div key={label} style={{ display: 'flex', alignItems: 'center', color: 'white', marginTop: '10px' }}>
+              <span style={{
+                backgroundColor: categoryColors[index],
+                width: '20px',
+                height: '20px',
+                display: 'inline-block',
+                marginRight: '10px',
+                borderRadius: '50%',
+              }}></span>
+              {label}: {value}%
+            </div>
+          );
+        })}
+      </div>
+    </div>
+
 
     </div>
   );
