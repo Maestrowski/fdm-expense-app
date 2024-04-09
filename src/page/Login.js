395684f2c466703f3431709760c2903a44f9a206
TheFarDoor <thebudderadam@gmail.com>
2024-04-09 16:11:28 +0100

    Add Login

--- src/page/Login.js
+++ src/page/Login.js
@@ -3,9 +3,10 @@ import { useTranslation } from 'react-i18next'; // Import the useTranslation hoo
 import { auth } from "../firebase";
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { Link } from "react-router-dom";
+import { SignUp } from "./SignUp";
 import AuthDetails from "./AuthDetails";
 
-const Login = () => {
+const Login = (props) => {
   const { t } = useTranslation(); // Destructure t function from useTranslation
 
   const [email, setEmail] = useState('');
@@ -22,8 +23,18 @@ const Login = () => {
   };
 
   return (
-    <div>
-      Login
+    <div className="auth-form-container">
+        <h2>{t("Login")}</h2>
+        <form className="login-form" onSubmit={login}>
+            <label htmlFor="email">email</label>
+            <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
+            <label htmlFor="password">password</label>
+            <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="********" id="password" name="password" />
+            <button type="submit">Log In</button>
+        </form>
+        <Link to="../signup">
+          <button className="link-btn">{t("Don't have an account? Register here.")}</button>
+        </Link>
     </div>
   );
 };
